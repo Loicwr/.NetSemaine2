b7965931b3a98c782bf45cb7099d9f6fe9cc05e1
Loicwr <nephtyse19@hotmail.fr>
2025-06-25 11:46:46 +0200

    Docs : Exercice 7.1 et 7.3

---
+++ Semaine2Exercice7/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Semaine2Exercice7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {                                           //EXERCICE 7.1
+
+            Func<int, int> triple = x => x * 3;
+
+            Console.WriteLine(triple(10));
+
+                                                    //EXERCICE 7.1
+
+
+                                                    //EXERCICE 7.3
+
+            List<int> nombres = new List<int> { 2, 5, 6, 7, 9 };
+
+            nombres.RemoveAll(x => x % 2 != 0);
+
+            Console.WriteLine(string.Join("-",nombres));
+                                                    
+                                                    //EXERCICE 7.3
+        }       
+
+    }
+}
