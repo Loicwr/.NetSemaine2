e94d94be53709399667273df2a143e407b985d39
Loicwr <nephtyse19@hotmail.fr>
2025-06-25 10:52:53 +0200

    Docs : Exercice 6

---
+++ Semaine2Exo3/Semaine2Exercice6/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Semaine2Exercice6
+{
+    internal class Program
+    {
+         
+        static void Main(string[] args)
+        {
+            int[] tableau = new int[] {5, 8 , 13};
+
+            AnalyseNombres(tableau);
+        }
+
+        static void AnalyseNombres(int[] tableau)
+        {
+            
+            bool EstPair(int x)
+            {
+                return x % 2 == 0;
+            }
+            
+            foreach(int nombre in tableau)
+            {
+
+                if (EstPair(nombre))
+                {
+                    Console.WriteLine($"{nombre} est pair");
+                }
+                else
+                {
+                    Console.WriteLine($"{nombre} est impair");
+                }
+
+                                    
+            }  
+
+        }
+    }
+}
