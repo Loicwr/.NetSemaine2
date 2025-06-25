915ca7025cadd8855343a343c66ffa758051d63a
Loicwr <nephtyse19@hotmail.fr>
2025-06-25 09:05:37 +0200

    Docs : Exercice 5

---
+++ Semaine2Exercice5/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Semaine2Exercice5
+{
+    internal class Program
+    {
+        void BonjourPersonnalise(string nom)
+        {
+            Console.WriteLine($"Bonjour {nom}");
+        }
+        static void BonjourGlobal()
+        {
+            Console.WriteLine("Bonjour les gens");
+        }
+        static void Main(string[] args)
+        {
+            Program.BonjourGlobal();
+            Program program = new Program();
+            program.BonjourPersonnalise("Alice");
+        }
+    }
+}
